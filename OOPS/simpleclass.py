5c32b2582fb146c8d73ee04d20fc90cd95050ffd
sanidhya12345 <sanidhya.varshney_cs19@gmail.com>
2026-02-14 09:35:10 +0530

    simple oops program to demonstrate how classes and objects will work in python

---
+++ OOPS/simpleclass.py
@@ -0,0 +1,13 @@
+#this is the way how the class will be created in python
+class Myclass:
+    x=5
+
+
+p1=Myclass() # this is the way how to initialize the objects of the class in python
+print(p1.x)
+
+#how to delete an object in python
+
+del p1
+# print(p1.x) here p1 is not defined a NameError will be raised because i have deleted the p1 object
+
